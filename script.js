a7762b7ed0a44238b7b3c96b516a883107d056d7
Nils <nmittel@protonmail.com>
2023-01-18 13:09:35 +0100

    v2

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+const OPACITY_STEP = 100;
+FIRST_TIMEOUT = 1500;
+SECOND_TIMEOUT = 4000;
+
 
 function init(){
-    let video = document.getElementById('video');
-    video.playbackRate =0.7;
+    // let video = document.getElementById('video');
+    // video.playbackRate =0.7;
+    let firstLine = document.getElementById('hey');
+    let secondLine = document.getElementById('secondLine');
+    firstLine.style.opacity = 0;
+    secondLine.style.opacity = 0;
+
+    Introduction();
+
+
+    function Introduction(){
+        fadeIn(firstLine, FIRST_TIMEOUT);
+        fadeIn(secondLine, SECOND_TIMEOUT);
+        
+    }
+
+    async function fadeIn(element, timeout) {
+        await new Promise(r => setTimeout(r, timeout));
+        for (i=0; i< OPACITY_STEP; ++i){
+            element.style.opacity = i/OPACITY_STEP;
+            await new Promise(r => setTimeout(r, 1));
+        }
+        
+    }
+
+    function show() {
+        
+        if (opacity < 1) {
+            opacity = opacity + 0.1;
+            
+        } 
+    }
 }
